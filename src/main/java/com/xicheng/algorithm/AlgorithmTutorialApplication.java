377cd4b57b4aaea78cffac42905cc91b595a5303
xichengxml <772685628@qq.com>
2019-04-30 21:38:48 +0800

    Initial commit

---
+++ src/main/java/com/xicheng/algorithm/AlgorithmTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.xicheng.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmTutorialApplication.class, args);
+    }
+
+}
